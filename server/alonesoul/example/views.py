470cda40349acbf20ae7561cd23d7ae3a4fce822
steptree <zhaodianle860315@gmail.com>
2017-05-06 22:52:16 +0800

    init server

---
+++ server/alonesoul/example/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from alonesoul.example.serializers import CategorySerializer, PostSerializer
+from alonesoul.example.models import Category, Post
+
+
+class CategoryViewSet(ViewSet):
+
+    def list(self, request):
+        queryset = Category.objects.all()
+        serializer = CategorySerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+    def retrieve(self, request, pk=None):
+        queryset = Category.objects.all()
+        item = get_object_or_404(queryset, pk=pk)
+        serializer = CategorySerializer(item)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        try:
+            item = Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            return Response(status=404)
+        serializer = CategorySerializer(item, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    def destroy(self, request, pk=None):
+        try:
+            item = Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            return Response(status=404)
+        item.delete()
+        return Response(status=204)
+
+
+class PostViewSet(ViewSet):
+
+    def list(self, request):
+        queryset = Post.objects.all()
+        serializer = PostSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+    def retrieve(self, request, pk=None):
+        queryset = Post.objects.all()
+        item = get_object_or_404(queryset, pk=pk)
+        serializer = PostSerializer(item)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        try:
+            item = Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            return Response(status=404)
+        serializer = PostSerializer(item, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    def destroy(self, request, pk=None):
+        try:
+            item = Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            return Response(status=404)
+        item.delete()
+        return Response(status=204)
